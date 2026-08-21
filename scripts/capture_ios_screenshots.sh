b463e29d93cc7b42a1edccef7c1fb8e0e1f1647e
Dospacite <dospacite@protonmail.com>
2026-08-21 15:12:55 +0300

    run simulator screenshot journeys in debug mode

--- scripts/capture_ios_screenshots.sh
+++ scripts/capture_ios_screenshots.sh
@@ -72,7 +72,7 @@ log "running $SCENARIO"
 (
   cd "$APP_DIR"
   APPRUNNER_SCREENSHOT_DIR="$OUTPUT_DIR" \
-    flutter drive --profile \
+    flutter drive --debug \
       --device-id "$DEVICE_ID" \
       --target "$SCENARIO" \
       --driver test_driver/apprunner_screenshots_driver.dart
